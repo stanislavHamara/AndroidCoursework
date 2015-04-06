e73b0bae387c0550e93602e7a042c7c2ebbb5847
stan <stanislav.hamara@gmail.com>
2015-04-06 14:53:54 +0100

    spinners change based on each other

--- ExpensesManager/app/src/main/java/com/stanislav/hamara/expensesmanager/SpinnerActivity.java
+++ ExpensesManager/app/src/main/java/com/stanislav/hamara/expensesmanager/SpinnerActivity.java
@@ -1,9 +1,13 @@
 package com.stanislav.hamara.expensesmanager;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by stan on 06/04/15.
@@ -11,13 +15,74 @@ import android.widget.AdapterView;
 public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
     public int subcategory;
+    public View mView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        Log.e("Spinner created", " ");
+
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         subcategory = position;
+        initializeSecondarySpinner(position);
+
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void initializeMainSpinner(View view){
+        mView = view;
+        Spinner mDescSpinner = (Spinner) view.findViewById(R.id.category_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(),
+                R.array.category_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mDescSpinner.setAdapter(adapter);
+        mDescSpinner.setOnItemSelectedListener(this);
+
+        //initialize the subcategory spinner based on what is selected in the first one
+        initializeSecondarySpinner(subcategory);
+
+
+    }
+
+    public void initializeSecondarySpinner(int sub){
+        Spinner mSubDescSpinner = (Spinner) mView.findViewById(R.id.subcategory_spinner);
+        ArrayAdapter<CharSequence> adapter;
+        switch (sub) {
+            case 0:
+                adapter = ArrayAdapter.createFromResource(mView.getContext(),
+                        R.array.meal_array, android.R.layout.simple_spinner_item);
+                break;
+            case 1:
+                adapter = ArrayAdapter.createFromResource(mView.getContext(),
+                        R.array.drink_array, android.R.layout.simple_spinner_item);
+                break;
+            case 2:
+                adapter = ArrayAdapter.createFromResource(mView.getContext(),
+                        R.array.transport_array, android.R.layout.simple_spinner_item);
+                break;
+            case 3:
+                adapter = ArrayAdapter.createFromResource(mView.getContext(),
+                        R.array.accomodation_array, android.R.layout.simple_spinner_item);
+                break;
+            case 4:
+                adapter = ArrayAdapter.createFromResource(mView.getContext(),
+                        R.array.accomodation_array, android.R.layout.simple_spinner_item);
+                mSubDescSpinner.setEnabled(false);
+                break;
+            default:
+                adapter = ArrayAdapter.createFromResource(mView.getContext(),
+                        R.array.category_array, android.R.layout.simple_spinner_item);
+                break;
+
+        }
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSubDescSpinner.setAdapter(adapter);
     }
 }
--- ExpensesManager/app/src/main/java/com/stanislav/hamara/expensesmanager/TabFragment.java
+++ ExpensesManager/app/src/main/java/com/stanislav/hamara/expensesmanager/TabFragment.java
@@ -18,14 +18,12 @@ public class TabFragment extends Fragment {
     private int mTabID;
     private Spinner mDescSpinner;
     private Spinner mSubDescSpinner;
-    private SpinnerActivity mSpinnerActivity = new SpinnerActivity();
-    private int subcategory;
+    private SpinnerActivity mSpinnerActivity;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         Bundle mBundle = getArguments();
-        subcategory = mSpinnerActivity.subcategory;
         mTabID = mBundle.getInt("tabID");
     }
 
@@ -40,9 +38,8 @@ public class TabFragment extends Fragment {
             case 1:
                 view = inflater.inflate(R.layout.fragment_tab_expenses, null);
                 //initialize the main spinner
-                initializeMainSpinner(view);
-                //initialize the subcategory spinner based on what is selected in the first one
-                initializeSecondarySpinner(view, subcategory);
+                mSpinnerActivity = new SpinnerActivity();
+                mSpinnerActivity.initializeMainSpinner(view);
                 break;
             case 2:
                 view = inflater.inflate(R.layout.fragment_tab_summary, null);
@@ -54,48 +51,6 @@ public class TabFragment extends Fragment {
         return view;
     }
 
-    public void initializeMainSpinner(View view){
-        mDescSpinner = (Spinner) view.findViewById(R.id.category_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(),
-                R.array.category_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mDescSpinner.setAdapter(adapter);
-        mDescSpinner.setOnItemSelectedListener(mSpinnerActivity);
-    }
 
-    public void initializeSecondarySpinner(View view, int sub){
-        mSubDescSpinner = (Spinner) view.findViewById(R.id.subcategory_spinner);
-        ArrayAdapter<CharSequence> adapter;
-        switch (sub) {
-            case 0:
-                adapter = ArrayAdapter.createFromResource(view.getContext(),
-                    R.array.meal_array, android.R.layout.simple_spinner_item);
-                break;
-            case 1:
-                adapter = ArrayAdapter.createFromResource(view.getContext(),
-                        R.array.drink_array, android.R.layout.simple_spinner_item);
-                break;
-            case 2:
-                adapter = ArrayAdapter.createFromResource(view.getContext(),
-                        R.array.transport_array, android.R.layout.simple_spinner_item);
-                break;
-            case 3:
-                adapter = ArrayAdapter.createFromResource(view.getContext(),
-                        R.array.accomodation_array, android.R.layout.simple_spinner_item);
-                break;
-            case 4:
-                adapter = ArrayAdapter.createFromResource(view.getContext(),
-                        R.array.accomodation_array, android.R.layout.simple_spinner_item);
-                mSubDescSpinner.setEnabled(false);
-                break;
-            default:
-                adapter = ArrayAdapter.createFromResource(view.getContext(),
-                        R.array.category_array, android.R.layout.simple_spinner_item);
-                break;
-
-        }
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSubDescSpinner.setAdapter(adapter);
-    }
 }
 
