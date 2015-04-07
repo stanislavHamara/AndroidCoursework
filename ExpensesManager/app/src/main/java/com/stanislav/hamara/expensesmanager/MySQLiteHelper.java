8e2a7a0562dc22ee42b57dff19852344128c6009
stan <stanislav.hamara@gmail.com>
2015-04-07 11:41:53 +0100

    SQLHelper class created

---
+++ ExpensesManager/app/src/main/java/com/stanislav/hamara/expensesmanager/MySQLiteHelper.java
@@ -0,0 +1,46 @@
+package com.stanislav.hamara.expensesmanager;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by stan on 07/04/15.
+ */
+public class MySQLiteHelper extends SQLiteOpenHelper {
+
+    public static final String TABLE_NAME = "task";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "desc";
+
+    private static final String DATABASE_NAME = "expenses.db";
+    private static final int DATABASE_VERSION = 1;
+
+    //creation query
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_NAME + "( " + COLUMN_ID
+            + " integer primary key autoincrement, " + COLUMN_NAME
+            + " text not null);";
+
+    public MySQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(MySQLiteHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+}
+
