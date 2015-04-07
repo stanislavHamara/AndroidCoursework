1be93b08e9275b8bbe4eaf1b4eff0ab0cccefe78
stan <stanislav.hamara@gmail.com>
2015-04-07 09:52:31 +0100

    populated the list of expenses with a mockup

--- ExpensesManager/app/src/main/java/com/stanislav/hamara/expensesmanager/TabFragment.java
+++ ExpensesManager/app/src/main/java/com/stanislav/hamara/expensesmanager/TabFragment.java
@@ -13,7 +13,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 /**
  * Created by stan on 06/04/15.
@@ -104,9 +107,26 @@ public class TabFragment extends Fragment {
                     }
                 });
                 break;
+
             case 2:
                 view = inflater.inflate(R.layout.fragment_tab_summary, null);
+                final ListView listView = (ListView) view.findViewById(R.id.purchases_list);
+
+                String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                        "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                        "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                        "Android", "iPhone", "WindowsMobile" };
+
+                final ArrayList<String> list = new ArrayList<>();
+                for (int i = 0; i < values.length; ++i) {
+                    list.add(values[i]);
+                }
+                final StableArrayAdapter adapter = new StableArrayAdapter(getActivity(),
+                        android.R.layout.simple_list_item_1, list);
+                listView.setAdapter(adapter);
                 break;
+
             default:
                 view = inflater.inflate(R.layout.fragment_tab_home, null);
                 break;
